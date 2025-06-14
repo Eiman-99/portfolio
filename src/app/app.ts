2fae37d0e28aad7c50891702266b16d1a0eb0086
unknown <eimanhamdy99@gmail.com>
2025-06-14 05:16:24 -0700

    create hero component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
